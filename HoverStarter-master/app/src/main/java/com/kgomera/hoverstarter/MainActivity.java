7ee0e8e07a347eedfd2548520bfed8014054ed75
KudzanaiGomera <kgomera@student.wethinkcode.co.za>
2020-03-31 23:50:27 +0200

    working

---
+++ HoverStarter-master/app/src/main/java/com/kgomera/hoverstarter/MainActivity.java
@@ -0,0 +1,60 @@
+package com.kgomera.hoverstarter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.hover.sdk.actions.HoverAction;
+import com.hover.sdk.api.Hover;
+import com.hover.sdk.api.HoverParameters;
+import com.hover.sdk.permissions.PermissionActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements Hover.DownloadListener {
+	private final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Hover.initialize(getApplicationContext(), this);
+
+        Button permissionsButton = findViewById(R.id.permissions_button);
+        permissionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PermissionActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
+
+	    Button button= (Button) findViewById(R.id.action_button);
+	    button.setEnabled(true);
+	    button.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+			    Intent i = new HoverParameters.Builder(MainActivity.this)
+					.request("bded38b3") // Add your action ID here
+                    .extra("amount", "50") // Uncomment and add your variables if any
+					.buildIntent();
+			    startActivityForResult(i, 0);
+		    }
+	    });
+    }
+
+	@Override public void onError(String message) {
+//		Toast.makeText(this, "Error while attempting to download actions, see logcat for error", Toast.LENGTH_LONG).show();
+		Log.e(TAG, "Error: " + message);
+	}
+
+	@Override public void onSuccess(ArrayList<HoverAction> actions) {
+//		Toast.makeText(this, "Successfully downloaded " + actions.size() + " actions", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "Successfully downloaded " + actions.size() + " actions");
+	}
+}
